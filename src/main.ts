d753b3749ee7c3ef095559416daaec43d4336518
Roseskk <oleg419583@yandex.ru>
2023-11-20 22:54:23 +0300

    swagger

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,7 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
       .setTitle('API Студенческий портал')
-      .setDescription('API')
       .setVersion('1.0')
-      .addTag('tag')
       .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
